80e25cd282e6f0be583d43fc42cdf5e1e7e3bb02
Amitabh-DevOps <amitabhdevops2024@gmail.com>
2025-02-10 10:16:31 +0300

    updated terraform.tf

--- terraform/terraform_resources/terraform.tf
+++ terraform/terraform_resources/terraform.tf
@@ -5,12 +5,14 @@ terraform {
       version = "5.86.0"
     }
   }
+
   backend "s3" {
     bucket = "github-actions-bucket"
     key = "terraform.tfstate"
     region = "eu-west-1"
     dynamodb_table = "github-actions-db"
   }
+  
 }
 # Configure the AWS Provider
 provider "aws" {
